c110dc2a626fb9930ed35a3a5cc17d563f7ea856
hawthornbunny <hawthornrabbit@gmail.com>
2017-11-05 21:56:21 +0000

    1.5.3

---
+++ data/js/unused_cards.js
@@ -0,0 +1,60 @@
+/**
+ * This class contains data for cards that are currently being left out of the database for various reasons; usually
+ * because they're beta versions of other cards, and including them would be redundant.
+ */
+var UNUSED_CARDS = [
+    {
+        'name': 'Fluttershy',
+        'image': 'Sorden/Uncategorized/Mcv5h.jpg',
+        'creator': 'Sorden',
+        'notes': 'One of several cards found on an old NEOGAF thread. These appear to be earlier versions of the cards that would eventually be finalized in Sorden\'s "Friendship is Magic the Gathering" set.',
+    },
+    {
+        'name': 'Rarity',
+        'image': 'Sorden/Uncategorized/ojiCJ.jpg',
+        'creator': 'Sorden',
+        'notes': 'One of several cards found on an old NEOGAF thread. These appear to be earlier versions of the cards that would eventually be finalized in Sorden\'s "Friendship is Magic the Gathering" set.',
+    },
+    {
+        'name': 'Pinkie Pie',
+        'image': 'Sorden/Uncategorized/T0xQt.jpg',
+        'creator': 'Sorden',
+        'notes': 'One of several cards found on an old NEOGAF thread. These appear to be earlier versions of the cards that would eventually be finalized in Sorden\'s "Friendship is Magic the Gathering" set.',
+    },
+    {
+        'name': '20% Cooler Dress',
+        'image': 'Sorden/Uncategorized/TIIyP.jpg',
+        'creator': 'Sorden',
+        'notes': 'One of several cards found on an old NEOGAF thread. These appear to be earlier versions of the cards that would eventually be finalized in Sorden\'s "Friendship is Magic the Gathering" set.',
+    },
+    {
+        'name': 'Celestia, Princess of Equestria',
+        'image': 'Sorden/Uncategorized/uwKbi.jpg',
+        'creator': 'Sorden',
+        'notes': 'One of several cards found on an old NEOGAF thread. These appear to be earlier versions of the cards that would eventually be finalized in Sorden\'s "Friendship is Magic the Gathering" set.',
+    },
+    {
+        'name': 'Applejack',
+        'image': 'Sorden/Uncategorized/vGt9f.jpg',
+        'creator': 'Sorden',
+        'notes': 'One of several cards found on an old NEOGAF thread. These appear to be earlier versions of the cards that would eventually be finalized in Sorden\'s "Friendship is Magic the Gathering" set.',
+    },
+    {
+        'name': 'Artful Dress',
+        'image': 'Sorden/Uncategorized/VlhqM.jpg',
+        'creator': 'Sorden',
+        'notes': 'One of several cards found on an old NEOGAF thread. These appear to be earlier versions of the cards that would eventually be finalized in Sorden\'s "Friendship is Magic the Gathering" set.',
+    },
+    {
+        'name': 'Rainbow Dash',
+        'image': 'Sorden/Uncategorized/VzMmq.jpg',
+        'creator': 'Sorden',
+        'notes': 'One of several cards found on an old NEOGAF thread. These appear to be earlier versions of the cards that would eventually be finalized in Sorden\'s "Friendship is Magic the Gathering" set.',
+    },
+    {
+        'name': 'Twilight Sparkle',
+        'image': 'Sorden/Uncategorized/y4wgg.jpg',
+        'creator': 'Sorden',
+        'notes': 'One of several cards found on an old NEOGAF thread. These appear to be earlier versions of the cards that would eventually be finalized in Sorden\'s "Friendship is Magic the Gathering" set.',
+    },
+];
